75ee580d9910f1c3a382125f2e1abeffdf47131f
fogelkuba <fogelkuba@gmail.com>
2018-09-02 12:51:25 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan('dev'));
