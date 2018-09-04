e53c1bc4ce82c8be4c595be7896dcca8c0c28190
fogelkuba <fogelkuba@gmail.com>
2018-09-04 19:44:25 +0200

    fix

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -17,15 +17,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'ng-amazon-client'`, async(() => {
+  it(`should have as title 'client'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-amazon-client');
+    expect(app.title).toEqual('client');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-amazon-client!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to client!');
   }));
 });
