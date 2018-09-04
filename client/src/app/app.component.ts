def9e509b0defb232f49d6814520730a0e02a59b
fogelkuba <fogelkuba@gmail.com>
2018-09-04 20:09:07 +0200

    fix

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  searchTerm:string = '';
-  isCollapsed:boolean = true;
+    searchTerm: string = '';
+    isCollapsed: boolean = true;
 
-  get token() {
-    return localStorage.getItem('token');
-  }
+    get token() {
+        return localStorage.getItem('token');
+    }
 
-  collapse() {
-      this.isCollapsed = !this.isCollapsed;
-  }
+    collapse() {
+        this.isCollapsed = !this.isCollapsed;
+    }
+
+    closeDropdown(dropdown) {
+        dropdown.close();
+    }
+
+    logout() {
+
+    }
+
+    search() {
+        
+    }
 }
