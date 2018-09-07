96f654ca191922b8c91a5731f31a3fc5bcbd621b
fogelkuba <fogelkuba@gmail.com>
2018-09-07 20:26:34 +0200

    fix

--- client/src/app/data.service.ts
+++ client/src/app/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Router} from "@angular/router";
+import {NavigationStart, Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +9,23 @@ export class DataService {
   messageType: string = 'danger';
 
   constructor( private router: Router) {
-    this.router.events.subscribe();
+    this.router.events.subscribe(event => {
+        if (event instanceof NavigationStart) {
+            this.message = '';
+        }
+     });
+  }
+
+  error(message: string) {
+      this.messageType = 'danger';
+      this.message = message;
+  }
+  success(message: string) {
+      this.messageType = 'success';
+      this.message = message;
+  }
+  warning(message: string) {
+      this.messageType = 'warning';
+      this.message = message;
   }
 }
