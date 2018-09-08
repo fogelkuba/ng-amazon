17fb83735d48508f0d7400c8a2bd9a6b041ca71f
fogelkuba <fogelkuba@gmail.com>
2018-09-08 13:20:29 +0200

    fix

--- server/routes/account.js
+++ server/routes/account.js
@@ -3,8 +3,12 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
-router.post('./signup', (req, res, next) => {
-    console.log(req.body);
+router.post('/signup', (req, res, next) => {
+    // console.log(req.body);
+    let user = new User();
+    user.name = req.body.name;
+    user.password = req.body.password;
+    user.mail = req.body.mail;
 });
 
 module.exports = router;
