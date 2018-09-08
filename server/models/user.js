842e65541201075adc3d3748e3bf647008754158
fogelkuba <fogelkuba@gmail.com>
2018-09-08 10:30:39 +0200

    fix

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bcrypt = require('bcrypt-nodejs');
+const crypto = require('crypto');
 
 const UserSchema = new Schema({
     email: {type: String, unique: true, lowercase: true},
@@ -21,18 +22,26 @@ const UserSchema = new Schema({
 });
 
 UserSchema.pre('save', function(next) {
-    var user = this;
+    let user = this;
 
     if (!user.isModified('password')) return next();
 
     bcrypt.hash(user.password, null, null, function (err, hash) {
         if (err) return next(err);
 
-        user.passdowr = hash;
+        user.password = hash;
         next();
     })
 });
 
 UserSchema.methods.comparePasswords = function(password) {
     return bcrypt.compareSync(password, this.password);
-}
+};
+
+UserSchema.methods.gravatar = function (size) {
+    if (!this.size) size = 200;
+    if (!this.email) return 'https://gravatar.com/avatar/?s' + size + '&d=retro';
+    let md5 = crypto.createHash('md5').update(this.email).digest('hex');
+
+    return 'https://gravatar.com/avatar/' + md5 + '?s' + size + '&d=retro';
+};
