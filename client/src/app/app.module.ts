13cb621e2d6d68b0ad5aeaf17fd1ef795b2f591d
fogelkuba <fogelkuba@gmail.com>
2018-09-12 23:04:14 +0200

    fix

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RegistrationComponent } from './registration/registration.component';
 import {RegistrationService} from "./registration/registration.service";
 import { LoginComponent } from './login/login.component';
 import {LoginService} from "./login/login.service";
+import {AuthGuardService} from "./auth-guard.service";
 
 @NgModule({
     declarations: [
@@ -30,7 +31,12 @@ import {LoginService} from "./login/login.service";
         HttpClientModule,
         NgbModule.forRoot()
     ],
-    providers: [RestApiService, DataService, RegistrationService, LoginService],
+    providers: [RestApiService,
+                DataService,
+                RegistrationService,
+                LoginService,
+                AuthGuardService
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
--- client/src/app/auth-guard.service.ts
+++ client/src/app/auth-guard.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot,
+        CanActivate,
+        Router,
+        RouterStateSnapshot} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (localStorage.getItem('token')) {
+      this.router.navigate(['/']);
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
