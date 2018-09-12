25be816ac586e27090f9a6aabdc0e5cc03c0e9d9
fogelkuba <fogelkuba@gmail.com>
2018-09-12 22:28:46 +0200

    fix

--- client/src/app/login/login.service.ts
+++ client/src/app/login/login.service.ts
@@ -1,12 +1,36 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {DataService} from "../data.service";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,  public data: DataService, private router: Router) {}
 
-  private url = '';
+    private url = 'http://localhost:3030/api/accounts/login';
+
+    post(userCredentials) {
+        return this.http.post(this.url, userCredentials)
+            .subscribe(
+                (response) => {
+                    if (response['success']) {
+                        localStorage.setItem('token', response['token']);
+                        this.data.success('Registration successful');
+                        console.log(response['message']);
+                        this.router.navigate(['/']);
+                    } else {
+                        const err = response['message'];
+                        console.log(err);
+                        this.data.error(err);
+                    }
+                },
+                (error) => {
+                    const err = error['message'];
+                    this.data.error(err);
+                }
+            );
+    }
 }
