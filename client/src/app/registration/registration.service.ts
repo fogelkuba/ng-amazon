0d77f01308a4363e6aafa47977036aa6fafcc41f
fogelkuba <fogelkuba@gmail.com>
2018-09-12 18:50:28 +0200

    fix

--- client/src/app/registration/registration.service.ts
+++ client/src/app/registration/registration.service.ts
@@ -1,5 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
     providedIn: 'root'
@@ -12,5 +13,6 @@ export class RegistrationService {
 
     post(credentials) {
         return this.http.post(this.url, credentials)
+            .pipe(response => response)
     }
 }
