3f3f78fa9300829d3ea2d4375108ab11e38fe49e
fogelkuba <fogelkuba@gmail.com>
2018-09-17 22:30:11 +0200

    fix

--- server/routes/account.js
+++ server/routes/account.js
@@ -121,18 +121,18 @@ router.route('/address')
         User.findOne({_id: req.decoded.user._id}, (err, user) => {
             if (err) return next(err);
 
-            if (req.body.addres1) user.address.addres1 = req.body.addres1;
-            if (req.body.address2) user.address.address2 = req.body.address2;
-            if (req.body.city) user.address.city = req.body.city;
-            if (req.body.state) user.address.state = req.body.state;
-            if (req.body.country) user.address.country = req.body.country;
-            if (req.body.postalCode) user.address.postalCode = req.body.postalCode;
+            if (req.body.adress1) user.adress.adress1 = req.body.adress1;
+            if (req.body.adress2) user.adress.adress2 = req.body.adress2;
+            if (req.body.city) user.adress.city = req.body.city;
+            if (req.body.state) user.adress.state = req.body.state;
+            if (req.body.country) user.adress.country = req.body.country;
+            if (req.body.postalCode) user.adress.postalCode = req.body.postalCode;
 
             if (Object.keys(req.body).length) {
                 user.save();
                 res.json({
                     success: true,
-                    message: 'User adress edited'
+                    message: 'User adresss edited'
                 })
             } else {
                 res.json({
