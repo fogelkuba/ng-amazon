b668d4f4cb1987709b590c7b4696c6113b524c48
fogelkuba <fogelkuba@gmail.com>
2018-09-22 15:12:20 +0200

    fix

--- server/routes/seller.js
+++ server/routes/seller.js
@@ -9,4 +9,17 @@ const multerS3 = require('multer-s3');
 const {accessKeyId, secretAccessKey} = config.aws;
 const s3 = new aws.S3({accessKeyId, secretAccessKey});
 
+const upload = multer({
+    storage: multerS3({
+        s3,
+        bucket: 'angular-amazono',
+        metadata: function (req, file, cb) {
+            cb(null, {fieldName: file.fieldName})
+        },
+        key: function (req, file, cb) {
+            cb(null, {fieldName: file.fieldName})
+        }
+    })
+});
+
 module.exports = router;
