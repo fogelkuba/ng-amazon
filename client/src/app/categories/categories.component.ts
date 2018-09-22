91f31bddded761ebea88b3383689f567b8d17926
fogelkuba <fogelkuba@gmail.com>
2018-09-22 21:29:00 +0200

    fix

--- client/src/app/categories/categories.component.ts
+++ client/src/app/categories/categories.component.ts
@@ -1,35 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DataService} from "../data.service";
 import {RestApiService} from "../rest-api.service";
 
 @Component({
-  selector: 'app-categories',
-  templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.scss']
+    selector: 'app-categories',
+    templateUrl: './categories.component.html',
+    styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
 
-  categories: any;
+    categories: any;
 
-  constructor(private data: DataService, private rest: RestApiService) { }
+    newCategory = '';
 
-  ngOnInit() {
-      this.rest.get('/api/categories')
-          .subscribe(
-              (response) => {
-                if (response['success']) {
-                  this.categories = response['categories'];
+    btnDisabled = false;
+
+    constructor(private data: DataService, private rest: RestApiService) {
+    }
+
+    ngOnInit() {
+        this.fetchCategories();
+    }
+
+    fetchCategories() {
+        this.rest.get('/api/categories')
+            .subscribe(
+                (response) => {
+                    if (response['success']) {
+                        this.categories = response['categories'];
+                    }
+                    else {
+                        const msg = response['message'];
+                        this.data.error(msg)
+                    }
+                },
+                (error) => {
+                    const err = error['message'];
+                    this.data.error(err);
                 }
-                else {
-                  const msg = response['message'];
-                  this.data.error(msg)
+            )
+    }
+
+    addCategory(){
+        this.btnDisabled = true;
+        const body = {
+            category: this.newCategory
+        };
+        this.rest.post('/api/categories', body)
+            .subscribe(
+                (response) => {
+                    if (response['success']) {
+                        const msg = response['message'];
+                        this.data.success(msg);
+                    }
+                    else {
+                        const msg = response['message'];
+                        this.data.error(msg);
+                    }
                 }
-              },
-              (error) => {
-                  const err = error['message'];
-                  this.data.error(err);
-              }
-          )
-  }
+            );
+        this.btnDisabled = false;
+    }
 
 }
