0d6d16eea5748673301baf137baaafe0800e3fa8
fogelkuba <fogelkuba@gmail.com>
2018-09-22 22:08:06 +0200

    fix

--- client/src/app/post-product/post-product.component.ts
+++ client/src/app/post-product/post-product.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "../data.service";
+import {RestApiService} from "../rest-api.service";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-post-product',
-  templateUrl: './post-product.component.html',
-  styleUrls: ['./post-product.component.scss']
+    selector: 'app-post-product',
+    templateUrl: './post-product.component.html',
+    styleUrls: ['./post-product.component.scss']
 })
 export class PostProductComponent implements OnInit {
 
-  constructor() { }
+    product = {
+        title: '',
+        price: 0,
+        categoryId: '',
+        description: '',
+        product_picture: null
+    };
 
-  ngOnInit() {
-  }
+    categories: any;
+    btnDisabled = false;
+
+    constructor(private data: DataService,
+                private  rest: RestApiService,
+                private router: Router
+    ) {
+    }
+
+    ngOnInit() {
+    }
 
 }
