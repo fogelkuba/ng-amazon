07b49c7b51195c94e3fe393c1d4f392b706538d2
fogelkuba <fogelkuba@gmail.com>
2018-09-22 22:36:16 +0200

    fix

--- client/src/app/post-product/post-product.component.ts
+++ client/src/app/post-product/post-product.component.ts
@@ -28,6 +28,82 @@ export class PostProductComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.fetchCategories();
+    }
+
+    fetchCategories() {
+        this.rest.get('/api/categories')
+            .subscribe(
+                (response) => {
+                    if (response['success']) {
+                        this.categories = response['categories'];
+                    }
+                    else {
+                        const msg = response['message'];
+                        this.data.error(msg)
+                    }
+                },
+                (error) => {
+                    const err = error['message'];
+                    this.data.error(err);
+                }
+            )
+    }
+
+    validate(product) {
+        let validation = false;
+        !product.title ? this.data.error('Please enter title') : '';
+        !product.price ? this.data.error('Please enter price') : '';
+        !product.categoryId ? this.data.error('Please select category') : '';
+        !product.description ? this.data.error('Please enter description') : '';
+        !product.product_picture ? this.data.error('Please select product image') : '';
+        if (product.title && product.price && product.categoryId && product.description && product.product_picture) {
+            validation = true;
+        }
+        return validation;
+    }
+
+    fileChange(event: any) {
+        this.product.product_picture = event.target.files[0];
+    }
+
+    postProduct() {
+        this.btnDisabled = true;
+
+        if (this.validate(this.product)) {
+            const form = new FormData();
+            for (const key in this.product) {
+                if (this.product.hasOwnProperty(key)) {
+                    if (key === 'product_picture') {
+                        form.append(
+                            'product_picture',
+                            this.product.product_picture,
+                            this.product.product_picture.name,
+                        );
+                    } else {
+                        form.append(key, this.product[key]);
+                    }
+                }
+            }
+            this.rest.post('/api/seller/products', form)
+                .subscribe(
+                    (response) => {
+                        if (response['success']) {
+                            this.categories = response['categories'];
+                        }
+                        else {
+                            const msg = response['message'];
+                            this.data.error(msg)
+                        }
+                    },
+                    (error) => {
+                        const err = error['message'];
+                        this.data.error(err);
+                    }
+                )
+        }
+
+        this.btnDisabled = false;
     }
 
 }
