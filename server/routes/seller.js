6d0d4ec48366f516cc366f3088ddb213d0989492
fogelkuba <fogelkuba@gmail.com>
2018-09-22 14:35:54 +0200

    fix

--- server/routes/seller.js
+++ server/routes/seller.js
@@ -1,11 +1,12 @@
+const config = require('../config');
 const router = require('express').Router();
 const Product = require('../models/product');
-const config = require('../config');
 
 const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 
 const {accessKeyId, secretAccessKey} = config.aws;
-
 const s3 = new aws.S3({accessKeyId, secretAccessKey});
+
+mudule.expoerts = router;
