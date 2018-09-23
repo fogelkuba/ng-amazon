4dc1422e48a7d060961cc36c6e400644793a82f4
fogelkuba <fogelkuba@gmail.com>
2018-09-23 18:20:01 +0200

    fix

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "../data.service";
+import {RestApiService} from "../rest-api.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    products: any;
 
-  ngOnInit() {
-  }
+    constructor(
+        private data: DataService,
+        private rest: RestApiService
+    ) {
+    }
+
+    ngOnInit() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        this.rest.get('/api/products')
+            .subscribe(
+                (response) => {
+                    response['success']
+                        ? this.products = response['product']
+                        : this.data.error('Could not fetch products');
+                },
+                (err) => {
+                    this.data.error(err['message']);
+                }
+            )
+
+    }
 
 }
