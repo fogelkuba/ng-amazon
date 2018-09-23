36c6e78aff6a4cc4ea03bbf11f9629311f83973f
fogelkuba <fogelkuba@gmail.com>
2018-09-23 16:27:17 +0200

    fix

--- client/src/app/category/category.component.ts
+++ client/src/app/category/category.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {DataService} from "../data.service";
+import {RestApiService} from "../rest-api.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
-  selector: 'app-category',
-  templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+    selector: 'app-category',
+    templateUrl: './category.component.html',
+    styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+    categoryId: any;
+    category: any;
+    page = 1;
 
-  ngOnInit() {
-  }
+    protected pageSize = 10;
+
+    constructor(private data: DataService,
+                private rest: RestApiService,
+                private route: ActivatedRoute) {
+    }
+
+    ngOnInit() {
+        this.route.params.subscribe(res => {
+            this.categoryId = res['id'];
+            this.getProducts();
+        })
+    }
+
+    getProducts(eveent ?:any) {
+        if (event) {
+            this.category = null;
+        }
+        this.rest.get(`/api/categories/${this.categoryId}?page=${this.page - 1}`)
+            .subscribe(
+                (response) => {
+                    response['success'] ? this.category = response : this.data.error(response['message'])
+                },
+                (err) => {
+                    this.data.error(err['message'])
+                }
+            )
+    }
+
+    get lower() {
+        return this.pageSize * (this.page - 1) + 1;
+    }
+
+    get upper() {
+        return Math.min(this.pageSize * this.page, this.category.totalProducts)
+    }
 
 }
