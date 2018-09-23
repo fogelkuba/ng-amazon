92716b7f64d53a0a38a444188dfd2133144f9a6e
fogelkuba <fogelkuba@gmail.com>
2018-09-23 14:13:42 +0200

    fix

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,6 +3,40 @@ const Category = require('../models/category');
 const Product = require('../models/product');
 const async = require('async');
 
+router.get('/products', (req, res, next) => {
+    const pageSize = 10;
+    const page = req.query.page;
+    async.parallel([
+        function (callback) {
+            Product.count({}, (err, count) => {
+                let totalProducts = count;
+                callback(err, totalProducts);
+            });
+        },
+        function(callback) {
+            Product.find({})
+                .skip(pageSize * page)
+                .limit(pageSize)
+                .populate('category')
+                .populate('owner')
+                .exec((err, products) => {
+                    if (err) return next(err);
+                    callback(err, products);
+                });
+        },
+    ], function(err, results) {
+        let totalProducts = results[0];
+        let products = results[1];
+        res.json({
+            success: true,
+            message: `All products`,
+            products,
+            totalProducts,
+            pages: Math.ceil((totalProducts / pageSize))
+        })
+    })
+});
+
 router.route('/categories')
     .get((req, res, next) => {
         Category.find({}, (err, categories) => {
@@ -26,7 +60,6 @@ router.route('/categories')
 router.get('/categories/:id', (req, res, next) => {
     const pageSize = 10;
     const page = req.query.page;
-
     async.parallel([
         function (callback) {
             Product.count({category: req.params.id}, (err, count) => {
@@ -64,7 +97,26 @@ router.get('/categories/:id', (req, res, next) => {
             pages: Math.ceil((totalProducts / pageSize))
         })
     })
+});
 
+router.get('/product/:id', (req, res, next) => {
+    Product.findById({ _id: req.params.id})
+        .populate('category')
+        .populate('owner')
+        .exec((err, product) => {
+            if (err) {
+                res.json({
+                    success: false,
+                    message: `Product with id:${req.params.id} was not found`
+                })
+            } else {
+                res.json({
+                    success: true,
+                    message: `Found product with id:${req.params.id}`,
+                    product
+                })
+            }
+        })
 });
 
 module.exports = router;
