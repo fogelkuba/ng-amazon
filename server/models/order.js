c18678f95eed515770a19501241cd4f0ab1f9c0b
fogelkuba <fogelkuba@gmail.com>
2018-10-09 21:50:15 +0200

    fix

--- server/models/order.js
+++ server/models/order.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const deepPopulate =require('mongoose-deep-populate')('mongoose')
 
 const OrderSchema = new Schema({
     owner: {type: Schema.Types.ObjectId, ref: 'User'},
@@ -9,5 +10,7 @@ const OrderSchema = new Schema({
         quantity: {type: Number, default: 1}
     }]
 });
+
+OrderSchema.plugin(deeppopulate);
 
 module.exports = mongoose.model('Order, OrderSchema');
